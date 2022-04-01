f1542a0031df1ee59db507ce41ce995519e9c891
Ronye <ronye.rocha@iterative.com.br>
2022-04-01 18:46:06 -0300

    variables and constant

---
+++ movInstruction.asm
@@ -0,0 +1,9 @@
+.name movInstruction
+
+.model small
+
+.data
+    C equ 10        ; constant
+    var1 db 12      ; size => 1 byte; decimal; range => -128 to 255
+    var2 dw 0a112h  ; size => 2 bytes; hexadecimal; range => -32768 to 65536
+
