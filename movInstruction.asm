b509cb12ef61fad450a7a45c21a50c1817a94108
Ronye <ronye.rocha@iterative.com.br>
2022-04-01 18:52:11 -0300

    first mov instruction

--- movInstruction.asm
+++ movInstruction.asm
@@ -6,4 +6,9 @@
     C equ 10        ; constant
     var1 db 12      ; size => 1 byte; decimal; range => -128 to 255
     var2 dw 0a112h  ; size => 2 bytes; hexadecimal; range => -32768 to 65536
+    
+.code
+    main proc
+        mov al, 15 ; moving value 15 to AL (low side of AX register)
+    
 
