0663a29d345fe7716ebf1905fc6ec4ce41040c84
Ronye <ronye.rocha@iterative.com.br>
2022-04-05 18:46:52 -0300

    permutation

--- permutation.asm
+++ permutation.asm
@@ -1,12 +1,21 @@
+.model small
 .data
     C EQU 15
     var1 db 12
-    var2db c
+    var2 db c
   
-.main proc
-    main
+.code
+    main proc
+        mov ax, @data
+        mov ds, ax
+        
+        mov var2, C
+        
+        mov al, var1
+        mov ah, var2
+        
+        mov var2, al
+        mov var1, ah
     
-    
-    
-    endp
+    main endp
 end
